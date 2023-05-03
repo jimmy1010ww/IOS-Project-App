39cc49a823fc6ee090d3db06dd24e4a30c362d64
jimmy1010ww <35855869+jimmy1010ww@users.noreply.github.com>
2023-05-03 13:53:55 +0800

    Finish check_moodle_login

--- app.py
+++ app.py
@@ -12,7 +12,16 @@ app = Flask(__name__)
 #當使用 POST 請求訪問 /hello 時，它將從請求表單中獲取 name 參數並回應訊息。
 
 flask_logger = logging.getLogger('flask')
+flask_logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter('[Flask Logger]%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+console_handler = logging.StreamHandler()
+console_handler.formatter = formatter
+console_handler.setLevel(logging.DEBUG)
+flask_logger.addHandler(console_handler)
 
+
+moodleBot_list = []
+userid_list = []
 
 @app.route('/hello', methods=['GET', 'POST'])
 def hello():
@@ -30,29 +39,46 @@ def hello():
 def check_moodle_login():
     #如果是POST請求，從請求獲取json來進行登入
     if request.method == 'POST':
-
         flask_logger.debug("[Receive] POST request]")
+        flask_logger.info("Client IP : {}".format(str(request.remote_addr)))
+        flask_logger.info("Clinet Port : {}".format(str(request.environ['REMOTE_PORT'])))
+
+        # 取得新的 userid
+
+        if userid_list == []:
+            userid_list.append(0)
+        else:   
+            userid_list.append(userid_list[-1]+1)
+        flask_logger.info("userid : {}".format(str(userid_list[-1])))
 
         #從請求獲取json
         data = request.get_json()
+
         #從json中獲取帳號密碼
         username = data['username']
         password = data['password']
         flask_logger.info("username : {} password: {}".format(str(username), str(password)))
         
         #宣告moodleLogin物件
-        moodle = MoodleBot()
-        result = moodle.login(username, password)
-
+        current_moodleBot = MoodleBot(username, password, userid_list[-1])
+        moodleBot_list.append(current_moodleBot)
         #如果登入成功，回傳json
-        if result:
-            flask_logger.info("Login Sucess")
-            return jsonify({'result': 'success'})
+        if moodleBot_list[userid_list[-1]].login():
+            flask_logger.info("Login Success")
+            flask_logger.info("userid : {}".format(str(userid_list[-1])))
+            flask_logger.debug("moodleBot_list length : {}".format(str(len(moodleBot_list))))
+
+            return_data = jsonify({'result': 'success',
+                            'userid': str(userid_list[-1])})
+            flask_logger.debug("return_data : {}".format(return_data.json))
+            return return_data
         else:
             flask_logger.warning("Login Failed")
+            flask_logger.debug("moodleBot_list length : {}".format(str(len(moodleBot_list))))
             return jsonify({'result': 'fail'})
     #如果不是POST請求，回傳錯誤
     else:
+        flask_logger.warning("[Receive] invalid request method")
         return jsonify({'result': 'invalid request method'})
     
 if __name__ == '__main__':
--- moodle_bot.py
+++ moodle_bot.py
@@ -6,26 +6,64 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
 class MoodleBot:
-    def __init__(self):
-        self.logger = logging.getLogger('moodle')
+    def __init__(self, username, password, bot_id):
+        # init variables
+        self.username = username
+        self.password = password
+        self.bot_id = bot_id
+        self.driver = None
+        
+        # Init logger 
+        
+        # create logger
+        self.logger = logging.getLogger('moodle.{}'.format(str(self.bot_id)))
+        # set logger level
         self.logger.setLevel(logging.DEBUG)
+        # create formatter
+        formatter = logging.Formatter('[Moodle Bot Logger]%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+        
+        # create console handler and set level to debug
         console_handler = logging.StreamHandler()
-        self.logger.addHandler(console_handler)
-
-        formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+        # add formatter to console_handler
         console_handler.formatter = formatter
+        # set console_handler level
         console_handler.setLevel(logging.DEBUG)
 
+        # add console handler to logger
+        self.logger.addHandler(console_handler)
+
+        self.logger.debug("Moodle Bot {} init...".format(str(self.bot_id)))
+        self.logger.debug("username : {} password: {}".format(str(self.username), str(self.password)))
+        self.initDriver()
+        self.logger.info("Moodle Bot {} init done".format(str(self.bot_id)))
+
+    def initDriver(self):
+        # check if driver is already created
+        try:
+            if self.driver != None:
+                self.driver = None 
+        # catch the driver not created exception
+        except Exception as e:
+            self.logger.warning(str(e))
+            self.logger.warning("driver not created")
+
+        # configure chrome driver options
         chrome_options = Options() 
-        # chrome_options.add_argument('--headless')  # 啟動Headless 無頭
+        chrome_options.add_argument('--headless')  # 啟動Headless 無頭
         chrome_options.add_argument('--disable-gpu') #關閉GPU 避免某些系統或是網頁出錯
         chrome_options.add_argument('--log-level=2') #INFO = 0 WARNING = 1 LOG_ERROR = 2 LOG_FATAL = 3 default = 0
         chrome_options.add_experimental_option('excludeSwitches', ['enable-automation'])
 
-        service = Service(executable_path= os.path.abspath("\driver\chromedriver"))
-        self.driver = webdriver.Chrome(service=service, options=chrome_options)
+        # configure chrome driver service
+        service = Service(executable_path=os.path.abspath("\driver\chromedriver"))
 
-    def login(self, email, password):
+        # create chrome driver
+        self.driver = webdriver.Chrome(service=service, options=chrome_options)
+        self.logger.debug("driver created")
+
+    def login(self):
+        self.logger.debug("login")
+
         login_url = 'https://moodle2.ntust.edu.tw/login/index.php'
         home_page_url = 'https://moodle2.ntust.edu.tw/my/'
         self.driver.get(login_url)
@@ -36,8 +74,8 @@ class MoodleBot:
         login_button = self.driver.find_element(By.ID, "loginbtn")
 
         # Enter login credentials
-        email_field.send_keys(email)
-        password_field.send_keys(password)
+        email_field.send_keys(self.username)
+        password_field.send_keys(self.password)
 
         login_button.click()
 
@@ -46,20 +84,20 @@ class MoodleBot:
 
             # look if there is a login error element
             if self.driver.current_url == login_url:
-                self.logger.warning("login url is the same\n")
+                self.logger.warning("login url is the same")
                 login_error_element = self.driver.find_element(By.XPATH, "//*[@id=\"region-main\"]/div/div[3]/div/div[1]/div")
                 self.logger.warning(login_error_element.text)
                 return False
             elif self.driver.current_url == home_page_url:
-                self.logger.info("login success\n")
+                self.logger.info("Login Success")
                 return True
 
-        except Exception as e:
+        except TimeoutError as e:
             self.logger.warning(str(e))
             self.logger.warning("Timeout")
             try:
                 if self.driver.current_url == login_url:
-                    self.logger.warning("login url is the same\n")
+                    self.logger.warning("login url is the same")
                     login_error_element = self.driver.find_element(By.XPATH, "//*[@id=\"region-main\"]/div/div[3]/div/div[1]/div")
                     self.logger.warning(login_error_element.text)
                     return False
@@ -67,6 +105,9 @@ class MoodleBot:
                 self.logger.warning(str(e))
                 self.logger.critical("Unexpeted error")
                 return False
-
+        except Exception as e:
+            self.logger.warning(str(e))
+            self.logger.critical("Unexpeted error")
+            return False
     def close(self):
         self.driver.quit()
